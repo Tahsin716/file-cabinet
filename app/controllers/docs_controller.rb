0726c231a8845b786e34725f05aabc627c863b3f
Tahsin Rashad <tahsin.rashad716@gmail.com>
2016-10-24 00:18:44 +0600

    Docs controller and routes

---
+++ app/controllers/docs_controller.rb
@@ -0,0 +1,31 @@
+class DocsController < ApplicationController
+
+	def index
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+
+		def doc_params
+		end
+
+		def find_doc
+		end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
 
   root to: 'welcome#index'
 
+  resources :docs
+
 end
