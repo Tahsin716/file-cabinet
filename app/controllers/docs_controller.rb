7fe08c5e7fb48ec16e445a0274c9b8941b566bb5
Tahsin Rashad <tahsin.rashad716@gmail.com>
2016-10-24 01:35:45 +0600

    Added Doc views and actions
    time_ago_in_words
    truncate
    /docs --> 'index'

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,18 +1,29 @@
 class DocsController < ApplicationController
 
+	before_action :find_doc, only: [:show, :edit, :update, :destroy]
+
 	def index
+		@docs = Doc.all.order("created_at DESC")
 	end
 
 	def show
 	end
 
 	def new
+		@doc = Doc.new
 	end
 
 	def edit
 	end
 
 	def create
+		@doc = Doc.new(doc_params)
+
+		if @doc.save
+			redirect_to @doc
+		else
+			render 'new'
+		end
 	end
 
 	def update
@@ -24,8 +35,10 @@ class DocsController < ApplicationController
 	private
 
 		def doc_params
+			params.require(:doc).permit(:title, :content)
 		end
 
 		def find_doc
+			@doc = Doc.find(params[:id])
 		end
 end
